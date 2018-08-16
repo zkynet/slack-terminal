db2fe76b10f9fb072e4985c3256fa9cd0402b566
zkynet <anon@zkynet.io>
2018-08-16 12:58:55 +0000

    adding support for local execution

--- main.go
+++ main.go
@@ -37,33 +37,15 @@ func main() {
 			message := strings.Replace(ev.Msg.Text, botTagString, "", -1)
 			//fmt.Println(msg.Data)
 			messageParts := strings.Split(message, " ")
-			fmt.Println()
 
-			var cmd *exec.Cmd
-			cmd = exec.Command(os.Getenv("SSH_COMMAND_PATH"),
-				os.Getenv("SSH_COMMAND_ARGS"),
-				"ssh -p "+os.Getenv("SSH_PORT")+" -i "+os.Getenv("SSH_KEY")+" "+os.Getenv("SSH_USER")+"@"+messageParts[1]+" 'sudo -u root "+strings.Join(messageParts[2:], " ")+";exit'")
-			fmt.Println("Registering command:" + messageParts[1] + " : " + strings.Join(messageParts[2:], " "))
-			var stdout, stderr []byte
-			var errStdout, errStderr error
-			stdoutIn, _ := cmd.StdoutPipe()
-			stderrIn, _ := cmd.StderrPipe()
-			cmd.Start()
-
-			go func() {
-				stdout, errStdout = copyAndCapture(os.Stdout, stdoutIn, rtm, ev)
-			}()
-
-			go func() {
-				stderr, errStderr = copyAndCapture(os.Stderr, stderrIn, rtm, ev)
-			}()
-
-			err := cmd.Wait()
-			if err != nil {
-				log.Fatalf("cmd.Run() failed with %s\n", err)
-			}
-			if errStdout != nil || errStderr != nil {
-				log.Fatalf("failed to capture stdout or stderr\n")
+			fmt.Println(messageParts[0])
+			fmt.Println(messageParts[1])
+			fmt.Println(messageParts[2])
+			fmt.Println(messageParts[3])
+			if messageParts[1] == "remote" {
+				go remoteExecution(messageParts, rtm, ev)
+			} else if messageParts[1] == "local" {
+				go localExecution(messageParts, rtm, ev)
 			}
 
 		default:
@@ -71,6 +53,51 @@ func main() {
 		}
 	}
 
+}
+
+func localExecution(messageParts []string, rtm *slack.RTM, ev interface{}) {
+
+	var cmd *exec.Cmd
+	cmd = exec.Command(messageParts[2], strings.Join(messageParts[3:], " "))
+	fmt.Println("Registering local command:" + strings.Join(messageParts[2:], " "))
+	copyAndCaptureOutput(cmd, rtm, ev)
+	cmd.Start()
+
+	err := cmd.Wait()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+
+}
+
+func remoteExecution(messageParts []string, rtm *slack.RTM, ev interface{}) {
+
+	var cmd *exec.Cmd
+	cmd = exec.Command(os.Getenv("SSH_COMMAND_PATH"),
+		os.Getenv("SSH_COMMAND_ARGS"),
+		"ssh -p "+os.Getenv("SSH_PORT")+" -i "+os.Getenv("SSH_KEY")+" "+os.Getenv("SSH_USER")+"@"+messageParts[2]+" 'sudo -u root "+strings.Join(messageParts[3:], " ")+";exit'")
+	fmt.Println("Registering command:" + messageParts[2] + " : " + strings.Join(messageParts[3:], " "))
+	copyAndCaptureOutput(cmd, rtm, ev)
+	cmd.Start()
+
+	err := cmd.Wait()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+
+}
+
+func copyAndCaptureOutput(cmd *exec.Cmd, rtm *slack.RTM, ev interface{}) {
+
+	stdoutIn, _ := cmd.StdoutPipe()
+	stderrIn, _ := cmd.StderrPipe()
+	go func() {
+		_, _ = copyAndCapture(os.Stdout, stdoutIn, rtm, ev)
+	}()
+
+	go func() {
+		_, _ = copyAndCapture(os.Stderr, stderrIn, rtm, ev)
+	}()
 }
 
 func copyAndCapture(w io.Writer, r io.Reader, rtm *slack.RTM, ev interface{}) ([]byte, error) {
@@ -82,13 +109,8 @@ func copyAndCapture(w io.Writer, r io.Reader, rtm *slack.RTM, ev interface{}) ([
 			d := buf[:n]
 			out = append(out, d...)
 			rtm.SendMessage(rtm.NewOutgoingMessage(string(d), ev.(*slack.MessageEvent).Channel))
-			//_, err := w.Write(d)
-			//if err != nil {
-			//	return out, err
-			//}
 		}
 		if err != nil {
-			// Read returns io.EOF at the end of file, which is not an error for us
 			if err == io.EOF {
 				err = nil
 			}
